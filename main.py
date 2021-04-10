5f65d1f341d9d25e34a887c2352cb9a06d829da7
Girish Badi <78522377+Girishbadi@users.noreply.github.com>
2021-04-10 12:26:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,137 +1,43 @@
-# from collections import UserDict, UserString
-import unittest
-from typing import List, Optional
-from fastapi import Request, Form
-from fastapi import Response
-
-
-from fastapi import Depends, FastAPI, HTTPException,status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-
-import secrets
-import uvicorn
-import crud, schemas
-from  models import Base
-from  database import SessionLocal, engine
-from fastapi.middleware.cors import CORSMiddleware
-
-Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-security = HTTPBasic()
+db = []
 
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-origins = [
-    "http://localhost",
-    "http://localhost:4200",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.middleware("http")
-async def db_session_middleware(request: Request, call_next):
-    response = Response("Internal server error", status_code=500)
-    try:
-        request.state.db = SessionLocal()
-        response = await call_next(request)
-    finally:
-        request.state.db.close()
-    return response
-
-# Dependency
-def get_db(request: Request):
-    return request.state.db
+class User(BaseModel):
+    user_id = str
+    username = "username"
+    first_name ="first_name"
+    last_name ="last_name"
+    email="someone1@example.com"
 
 
-@app.post("/users/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
 
-@app.post("/users/authenticate/", response_model=schemas.User)
-async def user_login(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    print("Authentication called")
-    print(user.username)
-    print(user.password)
-    db_user = crud.get_user_login(db,username = user.username, password = user.password)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="username and password not found")
-    return db_user
-
-# @app.post("/users/authenticate/", response_model=schemas.User)
-# async def user_login(user:schemas.UserBase, db : Session = Depends(get_db)):
-#     print("Authentication called")
-#     #print(user.username)
-#     #print(user.password)
-#     #console.log("Authentication called");
-#     db_user = crud.get_user_login(db,username = user.username, password = user.password)
-#     #db_user=crud.user_login(db,username=username,password=password);
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="usename or password not correct")
-#     return db_user
-
-# @app.post("/users/authenticate/", response_model=schemas.User)
-# async def user_login(user:schemas.UserBase, db : Session = Depends(get_db)):
-#     print("Authentication called")
-#     db_user = crud.get_user_login(db,username = username, password = password)
-#     #db_user=crud.user_login(db,username=username,password=password);
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="usename or password not correct")
-#     return db_user
-
-@app.get("/users/", response_model=List[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+@app.get('/')
+def index():
+    return {'key' : 'value'}
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-async def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user 
+@app.get('/Users')
+def get_Users():
+    return db
 
- 
-@app.get("/users/authenticate/",response_model=schemas.User)
-def get_user_login(username:str, password:str, db : Session = Depends(get_db)):
-    db_user = crud.get_user_login(db,username = username, password = password)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="credentials not found")
-    return db_user
+@app.get('/Users/{user_id}')
+def get_user_by_id(user_id: int):
+    return db[user_id-1]
 
+@app.post('/Users')
+def create_user(user:User):
+    db.append(user.dict())
+    return db[-1]
 
-@app.delete("/users/{user_id}", response_model=schemas.User)
-async def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.delete_user_by_id(db, user_id=user_id)
-    return db_user 
+@app.put("/Users/{user_id}")
+def update_Users(user_id: str, username: str, first_name: str, last_name: str, email:str):
+    results = {"username":username, "user_id": user_id, "first_name": first_name, "last_name": last_name,"email": email}
+    return results
 
-
-# @app.post("/users/authenticate/", response_model=schemas.User)
-# async def user_login(user: schemas.UserBase, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_login(db, username=user.username,password=user.password)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="username is not load")
-#     return crud.user_login(db_user, user=user.username)
-
-
-# async def user_login(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_login(db,username = user.username,password=user.password)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="usename or password not correct")
-#     return crud.create_user(db=db, user=user)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000 , reload=True, access_log=False)   
+@app.delete('/Users/{user_id}')
+def delete_user(user_id: int):
+    db.pop(user_id-1)
+    return{}
